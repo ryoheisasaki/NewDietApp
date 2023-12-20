8b578b13d982068795c5480440d8dd0dc32331dd
佐々木 <soltpeppersolt@softbank.ne.jp>
2023-12-20 22:37:53 +0900

    Initial Commit

---
+++ diet app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diet app
+//
+//  Created by 佐々木遼平 on 2023/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
