76b174332a6228dbf227df9f38c4a781c8c4013f
佐々木 <soltpeppersolt@softbank.ne.jp>
2023-12-24 20:53:54 +0900

    step3

--- diet app/CalendarViewController.swift
+++ diet app/CalendarViewController.swift
@@ -9,4 +9,22 @@ import UIKit
 
 class CalendarViewController: UIViewController {
     
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBAction func addbutton(_ sender: Any) {
+        transitionToEditorView()
+    }
+
+
+
+func configureButton() {
+        addButton.layer.cornerRadius = addButton.bounds.width / 2
+    }
+    
+    func transitionToEditorView() {
+        let storyboard = UIStoryboard(name: "EditorViewController", bundle: nil)
+        guard let editorViewController = storyboard.instantiateInitialViewController() as? EditorViewController else { return }
+        present(editorViewController, animated: true)
+    }
 }
+
